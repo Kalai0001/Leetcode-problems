23dd5d50a911ff5a1a40aa9e3507d49b3995f842
Kalai0001 <kalai1172000@gmail.com>
2026-04-07 20:44:07 +0530

    Add files via upload

---
+++ withoutoperatoradd.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class withoutoperatoradd {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int a=sc.nextInt();
+        int b=sc.nextInt();
+        if(b>0){
+            for(int i=0;i<b;i++){
+                a++;
+            }
+        }
+        else{
+            for(int i=0;i>b;i--){
+                a--;
+            }
+        }
+        System.out.println(a);
+        sc.close();
+    }
+}
